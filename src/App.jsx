dce29a46dbb6a5d1db467c19ac83f06711ecc136
Pedro Rabadan Ribeiro <pedrorribeiro@meta.com>
2025-06-09 10:39:28 -0700

    added inital footer jsx and css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import './App.css'
 
 const App = () => {
@@ -9,7 +10,7 @@ const App = () => {
       <main>
         <h1></h1>
       </main>
-      <footer></footer>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import '../styles/footer.css'
+
+const Footer = () => {
+    return (
+        <footer>
+            <h1>© 2025 Flixster</h1>
+        </footer>
+    )
+}
+
+export default Footer
