f1d832f4b4cb580c4a08847b1ea9dc621d4befb2
Pedro Rabadan Ribeiro <pedrorribeiro@meta.com>
2025-06-10 17:02:47 -0700

    added like feature for each card using font awesome packages

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,8 +1,12 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as faHeartSolid } from '@fortawesome/free-solid-svg-icons';
 import "../styles/movie-card.css"
 
 const MovieCard = (props) => {
 
+    const [liked, setLiked] = useState(false)
     const posterUrl = `https://image.tmdb.org/t/p/w500${props.moviePoster}`;
 
     const toggleModal = () => {
@@ -13,7 +17,21 @@ const MovieCard = (props) => {
         <div onClick={toggleModal} className="card-container">
             <img src={posterUrl} />
             <div className="card-info">
-                <h1>{props.movieTitle}</h1>
+                <div className="title-like-container">
+                    <h1 style={{width: '75%'}}>{props.movieTitle}</h1>
+                    <div>
+                        Like:
+                        <FontAwesomeIcon 
+                            style={liked ? {marginLeft: '5px', color: 'red'} : {marginLeft: '5px'}}
+                            icon={liked ? faHeartSolid : faHeartRegular} 
+                            onClick={event => {
+                                    event.stopPropagation()
+                                    setLiked(!liked)
+                                }
+                            }
+                        />
+                    </div>
+                </div>
                 <h4>Rating: {Number(props.movieRating.toFixed(1))}</h4>
             </div>
         </div>
